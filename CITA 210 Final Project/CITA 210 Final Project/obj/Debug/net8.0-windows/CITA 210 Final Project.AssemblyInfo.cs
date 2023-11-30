15d307e93fd0e2cafd95d1dbdf27f281d263f98e
Grace Fowler <f.llfr.mgrace@gmail.com>
2023-11-30 14:41:14 -0500

    .

--- CITA 210 Final Project/CITA 210 Final Project/obj/Debug/net8.0-windows/CITA 210 Final Project.AssemblyInfo.cs
+++ CITA 210 Final Project/CITA 210 Final Project/obj/Debug/net8.0-windows/CITA 210 Final Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CITA 210 Final Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4285fbc5c05b390af33fc1d22b533f0a68a8668")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d94d75518dbbc5055e4654022c7d1b5333f0799")]
 [assembly: System.Reflection.AssemblyProductAttribute("CITA 210 Final Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CITA 210 Final Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
